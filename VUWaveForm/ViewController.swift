42aefeb6f223c3453e702887a7d64f32999b655c
Venky Venkatakrishnan <eeshwar1@gmail.com>
2019-08-27 23:06:58 -0500

    Initial Commit

---
+++ VUWaveForm/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VUWaveForm
+//
+//  Created by Venky Venkatakrishnan on 8/27/19.
+//  Copyright © 2019 Venky UL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
