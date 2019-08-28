97f3b9b7aa9ff30340271238108fcc2a3ae8af95
Venky Venkatakrishnan <eeshwar1@gmail.com>
2019-08-28 00:11:33 -0500

    Initial Coding based on masaldana2/SoundCloudWaveformSwift

--- VUWaveForm/ViewController.swift
+++ VUWaveForm/ViewController.swift
@@ -7,13 +7,40 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var waveFormView: NSView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+       //  let url = Bundle.main.url(forResource: "sample2", withExtension: "m4a")
+        
+        let url = Bundle.main.url(forResource: "Sample1", withExtension: "mp3")
+        let file = try! AVAudioFile(forReading: url!)
+        guard let format = AVAudioFormat(commonFormat: .pcmFormatFloat32, sampleRate: file.fileFormat.sampleRate, channels: file.fileFormat.channelCount, interleaved: false) else {
+            print(" returning...")
+            return  }
+        
+        print(file.fileFormat.channelCount)
+        
+        let buf = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: UInt32(file.length))
+        
+        do {
+            try file.read(into: buf!)
+        }
+        catch {
+            print("Unable to read file: \(file)")
+        }
+        
+        readFile.arrayFloatValues = Array(UnsafeBufferPointer(start: buf?.floatChannelData?[0], count:Int(buf!.frameLength)))
+        
+        //self.view.addSubview(waveFormView)
+        // self.waveFormView.needsDisplay = true
+        
     }
 
     override var representedObject: Any? {
